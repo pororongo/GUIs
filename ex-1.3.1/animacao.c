021a1e60cda20356305200f9bc63c8fb6ea64b1c
rafa <rafaellagrillo@gmail.com>
2025-08-29 18:35:49 -0300

    exercicio ciclo funcionando

--- ex-1.3.1/animacao.c
+++ ex-1.3.1/animacao.c
@@ -4,10 +4,10 @@ int main (int argc, char* args[]){
 	SDL_Window* win = SDL_CreateWindow("Animacao", 
 					   SDL_WINDOWPOS_UNDEFINED,
 					   SDL_WINDOWPOS_UNDEFINED, 
-					   200, 100, SDL_WINDOW_SHOWN);
+					   250, 150, SDL_WINDOW_SHOWN);
 	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-	SDL_Rect r = {40,20, 10,10};
+	SDL_Rect r = {50,20, 50,50};
 	while(1){
 		SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0x00);
 		SDL_RenderClear(ren);
@@ -16,18 +16,19 @@ int main (int argc, char* args[]){
 		SDL_RenderPresent(ren);
 		SDL_Delay(500);
 		
-		if(r.x > 0){
-			r.x -= 5;
+		if(r.x <= 100 && r.y <= 20){
+		   	r.x += 5;
 		}
-		if(r.x < 0){
-			r.y -= 5;
-		}
-		if(r.x < 0 && r.y < 0){
-			r.x += 5;
-		}
-		if(r.x > 0 && r.y < 0){
+		if(r.x >= 100 && r.y < 60){
 			r.y += 5;
 		}
+		if(r.x > 50 && r.y >= 60){
+			r.x -= 5;
+		}
+		if(r.x <= 50 && r.y > 20){
+			r.y -= 5;
+		}
+	
 	}
 		
 }
